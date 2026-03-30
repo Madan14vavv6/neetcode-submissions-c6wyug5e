58870ecba68875c7901a6c70e74f40d9a81331ea
MADAN KUMAR T M <madankumartm00@gmail.com>
2026-03-30 21:47:19 +0530

    Add: can-place-flowers - submission-4

---
+++ Data Structures & Algorithms/can-place-flowers/submission-4.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int len = flowerbed.length;
+        int canPlace = 0;
+        if(len >1){
+            if(len % 2 != 0 && (flowerbed[0] == 1 || flowerbed[1] == 0)){
+                canPlace = (len/2)+1;
+            }
+            else{
+                canPlace = len/2;
+            }
+        } 
+        else{
+            if(flowerbed[0] == 0){
+                canPlace = 1;
+            }
+            else{
+                canPlace = 1;
+            }    
+        }       
+        int alreadyPlace = 0;
+        for(int i = 0; i < len; i++){
+            if(flowerbed[i]==1){
+                alreadyPlace++;
+            }
+        }
+        
+        System.out.println(canPlace+" "+alreadyPlace+" "+(canPlace - alreadyPlace));
+        if(((canPlace - alreadyPlace)-n) >= 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
